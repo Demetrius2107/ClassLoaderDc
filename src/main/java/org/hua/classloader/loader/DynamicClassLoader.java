e3547cb39ee5a712a209326328aa15ca9981a70c
ShuKun Li <2562017477@qq.com>
2024-04-24 20:29:59 +0800

    DynamicClassLoader完善

--- src/main/java/org/hua/classloader/loader/DynamicClassLoader.java
+++ src/main/java/org/hua/classloader/loader/DynamicClassLoader.java
@@ -1,4 +1,46 @@
 package org.hua.classloader.loader;
 
-public class DynamicClassLoader {
+import org.hua.classloader.core.Chameleon;
+import org.hua.classloader.core.ChameleonSrcCode;
+import org.hua.classloader.pipeline.ChainContext;
+import org.hua.classloader.pipeline.ChainProcess;
+
+import java.util.Map;
+
+public class DynamicClassLoader  extends ClassLoader implements ChainProcess<ChameleonSrcCode> {
+
+    public DynamicClassLoader(){
+        super(DynamicClassLoader.class.getClassLoader());
+    }
+
+    public Class<?> load(Map<String,byte[]> bytecode) throws ClassNotFoundException{
+        if (null != bytecode || bytecode.size() != 0){
+            /**
+             * 加载得到Class对象
+             */
+            final String name = bytecode.keySet().iterator().next();
+            byte[] buf = bytecode.get(name);
+            if(buf == null){
+                return super.findClass(name);
+            }
+            return new DynamicClassLoader().loadClassByte(buf);
+        } else {
+            throw new ClassNotFoundException("can not found class");
+        }
+    }
+
+    public Class loadClassByte(byte[] classBytes){
+        return defineClass(null,classBytes,0, classBytes.length);
+    }
+
+    @Override
+    public void process(ChainContext<ChameleonSrcCode> context) {
+        try{
+            final Class<?> load = load(context.getProcessModel().getClassMap());
+            context.getProcessModel().setChameleon((Chameleon) load.newInstance());
+        } catch (Exception e){
+            context.setNeedBreak(true);
+            context.setException(e);
+        }
+    }
 }

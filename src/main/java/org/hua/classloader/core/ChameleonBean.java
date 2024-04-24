bb6c6326be233e014de5fdece6886ba81e5ea2d4
ShuKun Li <2562017477@qq.com>
2024-04-24 18:13:52 +0800

    ChameleonHandler

--- src/main/java/org/hua/classloader/core/ChameleonBean.java
+++ src/main/java/org/hua/classloader/core/ChameleonBean.java
@@ -1,8 +1,43 @@
 package org.hua.classloader.core;
 
 public class ChameleonBean implements Chameleon{
+
+
+    private final Chameleon chameleon;
+
+    private final String javaSrc;
+
+    private final String className;
+
+    public ChameleonBean(Chameleon chameleon,String javaSrc,String className){
+        this.chameleon = chameleon;
+        this.javaSrc = javaSrc;
+        this.className = className;
+    }
+
     @Override
     public void process(Object object) {
-
+        chameleon.process(object);
     }
+
+    @Override
+    public String toString() {
+        return "ChameleonBean{" +
+                "javaSrc='" + javaSrc + '\'' +
+                ",className = '" + className +'\'' +
+                '}';
+    }
+
+    public String getJavaSrc(){
+        return javaSrc;
+    }
+
+    public Chameleon getChameleon(){
+        return chameleon;
+    }
+
+    public String getClassName(){
+        return className;
+    }
+
 }
--- src/main/java/org/hua/classloader/core/ChameleonHandler.java
+++ src/main/java/org/hua/classloader/core/ChameleonHandler.java
@@ -1,8 +1,21 @@
 package org.hua.classloader.core;
 
+import org.hua.classloader.service.ChameleonManagerService;
+
+import java.util.Collection;
+
 public class ChameleonHandler <T> implements ChameleonExec<T>{
+
+    private final ChameleonManagerService chameleonManger;
+
+    private ChameleonHandler(ChameleonManagerService chameleonManger){
+        this.chameleonManger = chameleonManger;
+    }
+
+
     @Override
     public void exec(String name, T data) {
+       /* final Collection<ChameleonBean> chameleonBeans = chameleonManger.get(name);*/
 
     }
 }
